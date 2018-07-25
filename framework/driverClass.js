080fa158ca26aa9771028f0d3f9025dcf7dc5e50
saroraqasource <sarora@qasource.com>
2018-07-25 17:32:41 +0530

    Update driverClass.js

--- framework/driverClass.js
+++ framework/driverClass.js
@@ -29,7 +29,9 @@ var buildDriver = function(browser) {
 	    case 'Firefox' :
 
 	    var firefox = require('selenium-webdriver/firefox');
-		var path = require('geckodriver.exe').path;
+		//var path = require('geckodriver').path;
+			var currentPath = process.cwd();
+			var path = currentPath + 'geckodriver';
 		var service = new firefox.ServiceBuilder(path).build();
 	  	return new webdriver.Builder().
 	    withCapabilities(webdriver.Capabilities.firefox()).
